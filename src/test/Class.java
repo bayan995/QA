20f0dcb9b918df58dd7326f38bb243dbb0c92e39
bayan995 <bayanmayyas14@gmail.com>
2022-11-15 00:36:11 +0200

    first commit

--- src/test/Class.java
+++ src/test/Class.java
@@ -16,6 +16,8 @@ public class Class {
 
 	public static void main(String[] args) throws IOException  {
 		
+		
+		
 	      System.setProperty("webdriver.chrome.driver", "C:\\Users\\Toshiba\\Desktop\\my new driver\\chromedriver.exe");
 		
 
@@ -29,19 +31,27 @@ public class Class {
           
          
           List <WebElement> thestudents = driver.findElements(By.tagName("option"));
-         int theTotalNumberOfStudent = thestudents.size();
-          System.out.println(theTotalNumberOfStudent+"the orginal number");
+        // int theTotalNumberOfStudent = thestudents.size();
+         // System.out.println(theTotalNumberOfStudent+"the orginal number");
           
-          int howManyItems =1;
+        //  int howManyItems =1;
       //    System.out.println(howManyItems+"this is the number of item i want removed");
           
-        		  for(int i=0 ;i <howManyItems ;i++) {
+        		  for(int i=0 ;i <thestudents.size() ;i++) {
+        			  
+        			  if(i%2==0) {
+        				  thestudents.get(i).click();
         			  
           driver.findElement(By.xpath("//*[@id=\"remove\"]")).click();
           
         		  }
-        		  
-        		  List<WebElement> theStudentAfterRemove = driver.findElements(By.tagName("option"));
+        			  else{
+        				System.out.println("delet"+thestudents.get(i).getText());  
+        				
+        			  }
+        			  
+        			  }
+        		//  List<WebElement> theStudentAfterRemove = driver.findElements(By.tagName("option"));
         		  
           
           //Date currentDate = new Date();
@@ -54,14 +64,14 @@ public class Class {
           
           //File Dest = new File(".//mypictures/"+TheAcctualDate+".png");
           //FileUtils.copyFile (SrcFile,Dest);
-        	int	acctualNumber=  theStudentAfterRemove.size();
+        	//int	acctualNumber=  theStudentAfterRemove.size();
         	
-            System.out.println(acctualNumber+"this is the new acctual number");
+            //System.out.println(acctualNumber+"this is the new acctual number");
             
-        	int	  expectedItem =theTotalNumberOfStudent-howManyItems;
+        	//int	  expectedItem =theTotalNumberOfStudent-howManyItems;
         	
-            System.out.println(expectedItem+"this is the number i have expected");
-          Assert.assertEquals(acctualNumber ,expectedItem);
+         //System.out.println(expectedItem+"this is the number i have expected");
+       //   Assert.assertEquals(acctualNumber ,expectedItem);
         
 	}
 
